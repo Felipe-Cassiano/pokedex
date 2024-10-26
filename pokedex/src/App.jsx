8596f175a3740433dc5c347bf5e915c0eba236dc
Felipe Cassiano <felipe3.cassiano@gmail.com>
2024-10-26 09:48:27 -0300

    Layout Modificado e adicionado mais features
    adicionado tipo do pokémon, background e mais

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,11 +1,9 @@
-import Header from './Components/Header/Header.jsx'
 import Body from './Components/Body/Body.jsx'
 
 function App() {
 
   return (
     <div>
-      <Header />
       <Body />
     </div>
   )
--- pokedex/src/Components/Body/Body.jsx
+++ pokedex/src/Components/Body/Body.jsx
@@ -2,29 +2,40 @@ import './Body.css'
 import './SearchBar.css'
 import './PokeInfo.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react';
+import { faMagnifyingGlass, faWeightHanging, faRuler } from '@fortawesome/free-solid-svg-icons'
+import { useState, useEffect } from 'react';
 
 
 function Body() {
-
-    //recebe o valor da imagem do pokémon escolhido
-    const [pkmnName, setPkmnName] = useState('')
-    const [pkmnImg, setPkmnImg] = useState('')
     // recebe valor dos status
-    const [pkmnStatus, setPkmnStatus] = useState({})
+    const [pkmnInfo, setPkmnInfo] = useState({})
     //decide qual pokémon vai ser escolhido
     const [pkmnId, setPkmnId] = useState('')
+
+    const [bgImg, setBgImg] = useState('')
+
+    useEffect(() => {
+        document.querySelector('.PokeInfo-container').style.backgroundImage = bgImg;
+      }, [bgImg]);
 
     //função assíncrona para dar fetch() na PokeAPI e receber o JSON do pokémon
     async function testeApi(pkmnId) {
         try {
             const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkmnId}`); //fetch() na API
             const dataPkmn = await res.json(); //transforma o fetch() em um objeto JSON
-            setPkmnImg(dataPkmn.sprites.front_default); //denomina o valor da imagem do pokémon
-            setPkmnName(dataPkmn.species.name)
             // recebe o valor dos status do pokémon
-            setPkmnStatus({
+            setPkmnInfo({
+                img: dataPkmn.sprites.front_default,
+
+                name: dataPkmn.species.name,
+
+                height: (dataPkmn.height) / 10,
+                weight: (dataPkmn.weight) / 10,
+
+                type0: await `/assets/types/${dataPkmn.types[0].type.name}.png`,
+                type1: await dataPkmn.types[1] ? `/assets/types/${dataPkmn.types[1].type.name}.png` : '',
+
+
                 hp: 'HP: ' + dataPkmn.stats[0].base_stat,
                 attack: 'Attack: ' + dataPkmn.stats[1].base_stat,
                 defense: 'Defense: ' + dataPkmn.stats[2].base_stat,
@@ -32,6 +43,16 @@ function Body() {
                 spDefense: 'Special Defense: ' + dataPkmn.stats[4].base_stat,
                 speed: 'Speed: ' + dataPkmn.stats[5].base_stat,
             })
+
+            switch (dataPkmn.types[0].type.name) {
+                case 'grass':
+                    setBgImg('https://i.pinimg.com/originals/07/e6/1f/07e61fbf05448a264881906c53b90367.gif')
+                    break;
+            
+                default:
+                    setBgImg('https://i.pinimg.com/originals/23/95/54/2395544bff0ddf8217c28c645cf604e5.gif')
+                    break;
+            }
         }
         catch (err) {
             console.error(err) //sinalizar caso aconteça algum erro
@@ -49,7 +70,8 @@ function Body() {
     };
 
     return (
-        <div className="Body-container">
+        <section className="Body-container">
+
             <div className="SearchBar-container">
                 <input type="text" className="SearchBar-input"
                     placeholder="Digite o nome de um Pokémon!"
@@ -59,18 +81,46 @@ function Body() {
                     onClick={handleSrc}>
                     <FontAwesomeIcon icon={faMagnifyingGlass} />
                 </button>
-                <p>{pkmnName}</p>
-                <img src={pkmnImg ? pkmnImg : 'https://placehold.co/96'} alt="" />
-                <ul>
-                    <li>{pkmnStatus.hp}</li>
-                    <li>{pkmnStatus.attack}</li>
-                    <li>{pkmnStatus.defense}</li>
-                    <li>{pkmnStatus.spAttack}</li>
-                    <li>{pkmnStatus.spDefense}</li>
-                    <li>{pkmnStatus.speed}</li>
-                </ul>
             </div>
-        </div>
+
+            <div className="PokeInfo-container">
+
+
+                <div className="PokeInfo-container-info">
+                    
+
+                    <div className="PokeInfo-container-info-info1">
+                        <img src={pkmnInfo.img ? pkmnInfo.img : 'https://placehold.co/96'} alt="" className='PokeInfo-container-info-info1-pkmn-img'/>
+
+                        <div className="PokeInfo-container-info-info1-types">
+                            <img src={pkmnInfo.type0} alt="" className='PokeInfo-container-info-info1-type'/>
+                            <img src={pkmnInfo.type1 ? pkmnInfo.type1 : ''} alt="" className='PokeInfo-container-info-info1-type'/>
+                        </div>
+
+                    </div>
+
+                    <div className="PokeInfo-container-info-info2">
+                        <div><FontAwesomeIcon icon={faWeightHanging} /> {pkmnInfo.weight} kg</div>
+                        <div><FontAwesomeIcon icon={faRuler} /> {pkmnInfo.height} m</div>
+                    </div>
+                </div>
+
+                <div className="PokeInfo-container-name">
+                    <p>{pkmnInfo.name}</p>
+                </div>
+
+                <div className="PokeInfo-container-stats">
+                    <ul>
+                        <li>{pkmnInfo.hp}</li>
+                        <li>{pkmnInfo.attack}</li>
+                        <li>{pkmnInfo.defense}</li>
+                        <li>{pkmnInfo.spAttack}</li>
+                        <li>{pkmnInfo.spDefense}</li>
+                        <li>{pkmnInfo.speed}</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
     )
 }
 
